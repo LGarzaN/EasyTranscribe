7f115abab3bfbf5bc3b3332afa25238690821269
Luis Eduardo Garza Naranjo <luis.garzanaranjo@gmail.com>
2023-09-23 21:13:17 -0600

    commit 2

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    
+    if request.method == "POST":
+        file = request.form["file"]
     return render_template("index.html")
 
 
